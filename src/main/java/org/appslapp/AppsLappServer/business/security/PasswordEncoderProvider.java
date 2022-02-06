74b1f304111b55d6408d737513213920ad9f493e
Jakub Kapitulcin <jakkappro@gmail.com>
2022-02-06 18:37:54 +0100

    final test i hope 0.3

--- src/main/java/org/appslapp/AppsLappServer/business/security/PasswordEncoderProvider.java
+++ src/main/java/org/appslapp/AppsLappServer/business/security/PasswordEncoderProvider.java
@@ -1,4 +1,4 @@
-package org.appslapp.AppsLappServer;
+package org.appslapp.AppsLappServer.business.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
