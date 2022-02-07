6235c67f529c96bf430e35b6ca8e4d6d4a504d63
Jakub Kapitulcin <jakkappro@gmail.com>
2022-02-07 19:25:27 +0100

    finally hope

--- src/main/java/org/appslapp/AppsLappServer/business/security/AuthenticationManager.java
+++ src/main/java/org/appslapp/AppsLappServer/business/security/AuthenticationManager.java
@@ -62,7 +62,7 @@ public class AuthenticationManager extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder manager) throws Exception {
         manager.userDetailsService(service).passwordEncoder(getEncoder());
         manager.inMemoryAuthentication()
-                .withUser("admin").password(getEncoder().encode(System.getenv().get("ADMIN_PASSWORD_FOR_APPSLAPP"))).authorities("ADMIN")
+                .withUser("admin").password(getEncoder().encode(System.getenv().get("ADMIN_PASSWORD"))).authorities("ADMIN")
                 .and()
                 .passwordEncoder(getEncoder());
     }
