335a74a4bb43efd904beefe6bd5456d57458d4d6
jakkappro <jakkappro@gmail.com>
2022-03-24 09:11:59 +0100

    lmao

--- src/main/java/org/appslapp/AppsLappServer/business/security/AuthenticationManager.java
+++ src/main/java/org/appslapp/AppsLappServer/business/security/AuthenticationManager.java
@@ -47,6 +47,7 @@ public class AuthenticationManager extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/api/management/getStudents").hasAnyAuthority("ADMIN", "LABMASTER")
                 .mvcMatchers("/api/management/createLab").hasAnyAuthority("LABMASTER")
                 .mvcMatchers("/api/management/getLabs").hasAnyAuthority("LABMASTER")
+                .mvcMatchers("/api/management/getAllExercises").hasAnyAuthority("ADMIN", "LABMASTER")
                 .mvcMatchers("/api/management/createExercise").hasAnyAuthority("ADMIN", "LABMASTER")
                 .mvcMatchers("/api/auth/promoteToLabmaster").hasAnyAuthority("ADMIN")
                 .mvcMatchers("/api/auth/createAdmins").permitAll()
