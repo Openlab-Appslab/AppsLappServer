829f35fc5521fc8a8d923533611419954777c066
jakkappro <jakkappro@gmail.com>
2022-05-19 08:45:17 +0200

    f

--- src/main/java/org/appslapp/AppsLappServer/business/security/AuthenticationManager.java
+++ src/main/java/org/appslapp/AppsLappServer/business/security/AuthenticationManager.java
@@ -35,7 +35,7 @@ public class AuthenticationManager extends WebSecurityConfigurerAdapter {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedHeaders(
                 List.of("Authorization", "Cache-Control", "Content-Type", "X-PT-SESSION-ID", "NGSW-BYPASS"));
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
         config.setAllowCredentials(true);
         config.setExposedHeaders(List.of("Authorization"));
