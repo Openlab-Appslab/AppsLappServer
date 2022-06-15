61391e1ab5b09f395170cbf9db5c369af485a656
jakkappro <jakkappro@gmail.com>
2022-06-15 10:00:53 +0200

    hmm

--- src/main/java/org/appslapp/AppsLappServer/business/mappers/StudentMapper.java
+++ src/main/java/org/appslapp/AppsLappServer/business/mappers/StudentMapper.java
@@ -3,10 +3,13 @@ package org.appslapp.AppsLappServer.business.mappers;
 import org.appslapp.AppsLappServer.business.Dto.StudentDto;
 import org.appslapp.AppsLappServer.business.pojo.users.user.User;
 
+import java.util.ArrayList;
+
 public class StudentMapper {
     public static StudentDto map(User student) {
         StudentDto studentDto = new StudentDto();
         studentDto.setName(student.getUsername());
+        studentDto.setExercises(new ArrayList<>());
         for (var i : student.getLab().getGroupOfExercises()) {
             for (var j : i.getExercises()) {
                 studentDto.getExercises().add(ExerciseStudentMapper.map(j));
