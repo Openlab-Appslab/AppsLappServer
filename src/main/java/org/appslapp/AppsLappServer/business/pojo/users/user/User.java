94200de7f5d97b5cc5925f845d3bd965ee474d40
jakkappro <jakkappro@gmail.com>
2022-06-15 09:57:33 +0200

    heh

--- src/main/java/org/appslapp/AppsLappServer/business/pojo/users/user/User.java
+++ src/main/java/org/appslapp/AppsLappServer/business/pojo/users/user/User.java
@@ -8,11 +8,9 @@ import org.appslapp.AppsLappServer.business.pojo.lab.Lab;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import java.util.List;
 
 @Entity(name = "user_data")
 @Getter
@@ -27,5 +25,6 @@ public class User extends org.appslapp.AppsLappServer.business.pojo.users.entity
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "lab_id")
+    @LazyCollection(LazyCollectionOption.FALSE)
     private Lab lab;
 }
