d7329119941d1b1b356a626c3e91cf9bde86ae75
jakkappro <jakkappro@gmail.com>
2023-01-26 08:02:47 +0100

    test stuff XD

--- src/main/java/org/appslapp/AppsLappServer/business/pojo/users/user/User.java
+++ src/main/java/org/appslapp/AppsLappServer/business/pojo/users/user/User.java
@@ -10,10 +10,7 @@ import org.appslapp.AppsLappServer.business.pojo.Lab;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "user_data")
@@ -32,8 +29,7 @@ public class User extends org.appslapp.AppsLappServer.business.pojo.users.entity
     private List<Exercise> doneExercises;
 
     @JsonIgnore
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "lab_id")
-    @LazyCollection(LazyCollectionOption.FALSE)
     private Lab lab;
 }
