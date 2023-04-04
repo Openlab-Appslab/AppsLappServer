c3059c12da6eaf32fd11e96d5d5e368541f1ce1e
jakkappro <jakkappro@gmail.com>
2023-04-04 08:51:36 +0200

    sad;lfka

--- src/main/java/org/appslapp/AppsLappServer/business/mappers/GroupMapper.java
+++ src/main/java/org/appslapp/AppsLappServer/business/mappers/GroupMapper.java
@@ -5,6 +5,7 @@ import org.appslapp.AppsLappServer.business.pojo.GroupOfExercises;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class GroupMapper {
     public static GroupOfExercisesDto map(GroupOfExercises student) {
@@ -16,7 +17,7 @@ public class GroupMapper {
         groupDto.setName(student.getName());
         groupDto.setMaxStars(student.getMaxStars());
         groupDto.setMinStars(student.getMinStars());
-        var deadline = LocalDateTime.parse(student.getDeadline());
+        var deadline = LocalDateTime.parse(student.getDeadline(), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
         var now = LocalDateTime.now();
 
         var days = Duration.between(deadline, now).toDays();
